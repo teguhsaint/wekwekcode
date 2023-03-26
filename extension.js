4483c9313cb11da707fd8abd0e6949b889c18d73
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2023-03-26 23:35:24 +0700

    d

--- extension.js
+++ extension.js
@@ -4,9 +4,11 @@ const https = require("https");
 const fs = require("fs");
 
 // MODULE
-const buat_view = require("./lib/buat_view_ci4");
+const ci4_view = require("./lib/ci4_view");
 const start_phpserver = require("./lib/start_phpserver");
 const download_template = require("./lib/download_template");
+const ci4_controller = require("./lib/ci4_controller");
+const ci4_route = require("./lib/ci4_route");
 
 // END MODULE
 
@@ -87,17 +89,17 @@ function activate(context) {
   });
 
   let ci_controller = vscode.commands.registerCommand("wekwekcode.ci_controller", function () {
-    cicontroller_func();
+    ci4_controller.cicontroller_func();
   });
 
   let ci_routes = vscode.commands.registerCommand("wekwekcode.ci_routes", function () {
-    tambah_route();
+    ci4_route.tambah_route();
   });
 
   let ci_view_extend = vscode.commands.registerCommand("wekwekcode.ci_view_extend", async function () {
     const nama_file = await vscode.window.showInputBox({ placeHolder: "Masukkan Nama File Template" });
     if (nama_file) {
-      buat_view.buat_views(nama_file);
+      ci4_view.buat_views(nama_file);
     }
   });
 
@@ -111,99 +113,3 @@ module.exports = {
   activate,
   deactivate,
 };
-
-function cicontroller_func() {
-  const dir_controller = path.join(direktori_user, "app/Controllers/");
-
-  async function hh() {
-    const nama_file = await vscode.window.showInputBox({ title: "Nama File Controller tanpa .php tidak boleh ada spasi" });
-    function capitalize(s) {
-      return s[0].toUpperCase() + s.slice(1);
-    }
-    if (!nama_file) {
-      vscode.window.showErrorMessage("Nama Tidak Boleh Kosong");
-    } else {
-      const file_Controller = fs.createWriteStream(dir_controller + "/" + capitalize(nama_file.replaceAll(" ", "")) + ".php", (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-
-      const isi_controller = `<?php
-
-namespace App\\Controllers;
-
-class ${capitalize(nama_file.replaceAll(" ", ""))} extends BaseController
-{
-    public function index()
-    {
-        // return view('');
-    }
-}
-`;
-      file_Controller.write(isi_controller, (err) => {
-        if (err) {
-          file_Controller.close();
-          console.log(err);
-        } else {
-          file_Controller.close();
-          vscode.window.showInformationMessage("Done " + nama_file);
-          const openPath = vscode.Uri.file(path.join(dir_controller, capitalize(nama_file) + ".php"));
-          vscode.commands.executeCommand("vscode.open", openPath);
-        }
-      });
-    }
-  }
-  hh();
-}
-
-function tambah_route() {
-  const routes_path = path.join(direktori_user, "app/Config/Routes.php");
-
-  async function tambah() {
-    const route_path = await vscode.window.showInputBox({ title: "Masukkan link tujuan" });
-    const controller_tujuan = await vscode.window.showInputBox({ title: "Masukkan Controller Tujuan" });
-
-    if (!route_path) {
-      vscode.window.showErrorMessage("Path route tidak boleh kosong");
-    } else {
-      const new_route = `$routes->get('/${route_path}','${controller_tujuan}::index');\n`;
-
-      fs.readFile(routes_path, "utf8", function (err, data) {
-        if (err) {
-          console.log(err);
-          vscode.window.showErrorMessage("Gagal membaca file routes.php");
-        } else {
-          // Cari baris yang ingin ditambahkan setelahnya
-          const target_line = `$routes->get('/', 'Home::index');`;
-
-          // Split data berdasarkan baris
-          const lines = data.split("\n");
-
-          // Cari indeks baris yang ingin ditambahkan setelahnya
-          const target_index = lines.findIndex((line) => line.includes(target_line));
-
-          if (target_index === -1) {
-            vscode.window.showErrorMessage("Tidak dapat menemukan baris target");
-            return;
-          }
-
-          // Tambahkan baris baru setelah baris target
-          lines.splice(target_index + 1, 0, new_route);
-
-          // Gabungkan kembali data menjadi string dan tulis ke file
-          fs.writeFile(routes_path, lines.join("\n"), function (err) {
-            if (err) {
-              console.log(err);
-              vscode.window.showErrorMessage("Gagal menambahkan route");
-            } else {
-              vscode.window.showInformationMessage("Berhasil menambahkan route");
-            }
-          });
-        }
-      });
-    }
-  }
-
-  tambah();
-}
